fae05d247313408142b4886cbf461edc35655bb0
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-03 20:18:52 -0600

    add flake outputs

--- flake.nix
+++ flake.nix
@@ -2,26 +2,20 @@
   description = "Nix flakes that aggregates assets from multiple sources.";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    haumea.url = "github:nix-community/haumea/v0.2.2";
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    self,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs self;}
-    {
-      debug = true;
-      systems = ["x86_64-linux"];
-      perSystem = {pkgs, ...}: {
-        packages = {
-          wallpapers = import ./wallpapers pkgs;
-          themes = import ./themes pkgs;
-        };
+  outputs = inputs: {
+      wallpapers = inputs.haumea.lib.load {
+        src = ./wallpapers;
+        loader = inputs.haumea.lib.matchers.extension ["png" "jpg" "jpeg"];
+        inputs = {inherit (inputs.nixpkgs) lib;};
       };
-      flake = {
-      };
+      themes = inputs.haumea.lib.load {
+        src = ./themes;
+        loader = inputs.haumea.lib.matchers.extension ["yaml"];
+        inputs = {inherit (inputs.nixpkgs) lib;};
     };
+  };
 }
