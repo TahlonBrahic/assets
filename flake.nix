09a4ce091651466de456bf8cae45e4e26a8937e3
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-07 07:10:32 -0600

    almost working again

--- flake.nix
+++ flake.nix
@@ -2,25 +2,21 @@
   description = "Nix flakes that aggregates assets from multiple sources.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     haumea.url = "github:nix-community/haumea/v0.2.2";
   };
 
   outputs = inputs: {
     wallpapers = inputs.haumea.lib.load {
       src = ./wallpapers;
-      loader = inputs.haumea.lib.matchers.extension ["png" "jpg" "jpeg"];
-      inputs = {inherit (inputs.nixpkgs) lib;};
+      loader = inputs.haumea.lib.matchers.always;
     };
     themes = inputs.haumea.lib.load {
       src = ./themes;
-      loader = inputs.haumea.lib.matchers.extension ["yaml"];
-      inputs = {inherit (inputs.nixpkgs) lib;};
+      loader = inputs.haumea.lib.matchers.always;
     };
     gifs = inputs.haumea.lib.load {
       src = ./gifs;
       loader = inputs.haumea.lib.matchers.always;
-      inputs = {inherit (inputs.nixpkgs) lib;};
     };
   };
 }
